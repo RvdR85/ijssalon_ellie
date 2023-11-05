49fed13fdbef1b99c814629ceebd30f88840b210
Rian van de Ruitenbeek <rianvierwind15@hotmail.com>
2023-11-05 07:49:35 +0100

    reclame toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,10 @@
+def mijn_functie_1(mijn_int):
+    return mijn_int * mijn_int
+print (mijn_functie_1(4))
+
+def mijn_functie_2(a,b):
+    print (a+b)
+    print (a-b)
+    print (a*b)
+    print (a/b)
+print (mijn_functie_2(12,3))
---
+++ reclame.py
@@ -0,0 +1,41 @@
+from algemene_functies.py import mijn_functie_2
+
+from statistics import mean
+
+
+def aanbieding_1(smaak, prijs, korting):
+    korting = (prijs-(prijs*korting))
+    return (f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {korting} euro.") 
+
+print (aanbieding_1("aardbei", 4, 0.1))
+
+
+def inkomsten_totaal (inkomsten, btw):
+    totaal = 0
+    for nr in inkomsten:
+        totaal += nr
+    bedrag = (totaal*btw)
+    return (f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {bedrag} euro btw betaald dient te worden") 
+
+inkomsten =[220,430,125,160,205,90,345]
+print (inkomsten_totaal(inkomsten,0.09))
+
+def laag_en_hoog(mijn_lijst):
+    return (max(mijn_lijst), min(mijn_lijst))
+mijn_lijst = [220,430,125,160,205,90,345]
+print (laag_en_hoog(mijn_lijst))
+
+def gemiddelde(mijn_lijst):
+    return (f"De gemiddelde inkomsten deze week zijn {mean(mijn_lijst)} euro.")
+print (gemiddelde(mijn_lijst))
+
+def meervoudig(invoer_lijst):
+    return laag_en_hoog(invoer_lijst)
+invoer_lijst = [10,5,3,2,1,2,9]
+print (meervoudig(invoer_lijst))
+
+def combinatie(invoer_lijst_2):
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    uitvoer = mijn_functie_2(korte_lijst[0],[1])
+    return uitvoer
+     
